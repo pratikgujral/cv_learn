8a0621e19399880944c1a64954334c65a2e90be7
pratikgujral <pratikgujral@gmail.com>
2018-01-02 15:06:38 +0530

    Added imageDifference.m

--- matlab/imageDifference.m
+++ matlab/imageDifference.m
@@ -12,7 +12,11 @@ figure, imshow(bicycle - dolphin);
 
 % Note: If difference leads to a value < 0, then value is truncated to zero
 % Hence, subtraction is leading to loss of information. 
-% To preserve image difference, either convert to float or us ethe
+% To preserve image difference, either convert to float or use the
 % following
 sub2 = (dolphin - bicycle) + (bicycle - dolphin);
 figure, imshow(sub2);
+
+% Can also use the imabsdiff function from the image package
+pkg load image;
+figure, imshow(imabsdiff(dolphin, bicycle)); % Order does not matter
